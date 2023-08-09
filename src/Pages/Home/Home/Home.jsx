a0a1b0ebbd589f07d19096a9ef35c2c8794c3aee
masudra <masudrana6278312@gmail.com>
2023-08-09 19:52:20 +0600

    project setup

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>this is home</h1>
+
+            <button className="btn btn-primary">Primary</button>
+            <button className="btn btn-primary">Primary</button>
+
+            
+        </div>
+    );
+};
+
+export default Home;
