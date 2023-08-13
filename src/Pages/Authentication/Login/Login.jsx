201aa9437ccf6d17e779731df39f826527f90543
masudra <masudrana6278312@gmail.com>
2023-08-13 21:30:03 +0600

    login and register pages add

---
+++ src/Pages/Authentication/Login/Login.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+
+
+const Login = () => {
+    const handelLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(password,email);
+    }
+    
+
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200 ">
+  <div className="hero-content flex-col lg:flex-row-reverse w-full">
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div className="card-body">
+        <form onSubmit={handelLogin}>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="Enter Your Email" name="email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="Enter Your Password" name="password" className="input input-bordered" />
+          <label className="label">
+           
+
+
+           <p> <span>You don,t have an account </span> <Link className="link-hover ml-10 text-xl text-purple-600" to='/register' > Regster </Link></p> 
+          </label>
+          
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn text-white bg-purple-800 ">Login</button><br/>
+          <button  className="btn text-white bg-purple-800 "> <FaGoogle></FaGoogle>Login With Google</button>
+          
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+        </div>
+    );
+};
+
+export default Login;
--- src/Pages/LandingPage/Shared/Navbar/Navbar.jsx
+++ src/Pages/LandingPage/Shared/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 function Navbar() {
 
@@ -8,7 +9,7 @@ function Navbar() {
                     <img className='w-full' src='https://i.ibb.co/WpJvMk4/9270632-02.png' alt="Logo" />
                 </div>
                 <div className='mr-10'>
-                    <button className='uppercase bg-[#22225a] px-5 py-2 rounded-lg font-medium'>Login</button>
+                    <Link to='/login'><button className='uppercase bg-[#22225a] px-5 py-2 rounded-lg font-medium'>Login</button></Link>
                 </div>
             </div>
         </div>
