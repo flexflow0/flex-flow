72aa58356f29396a239547010af7a29cc17e44c4
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2023-08-18 11:16:30 +0600

    implement payment

--- src/Pages/SubscriptionPlans/Payment/Payment.jsx
+++ src/Pages/SubscriptionPlans/Payment/Payment.jsx
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom";
 
+// import useSelectedData from '../../../../Hooks/useSelectedData';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe(`${import.meta.env.VITE_PK}`);
 const Payment = () => {
+    // const [cart] = useSelectedData()
+    // const total = cart.reduce((sum, item) => item.price + sum, 0);
+    // const amount = total * 100
+    // console.log(amount);
+
     return (
-        <div>
-            <h1>Payment</h1>
-            <Link to='/movieslist'> <button className="btn text-white bg-purple-800 ">Next</button><br />
-                  </Link>
-            
+        <div className='w-[97%] md:w-[90%] mx-auto '>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
