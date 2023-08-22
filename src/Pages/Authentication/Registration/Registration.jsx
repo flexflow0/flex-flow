7c6afa22a220eb459efca4dc5b4c7a11f490035b
masudra <masudrana6278312@gmail.com>
2023-08-22 23:15:23 +0600

    login page show password and reset password add

--- src/Pages/Authentication/Registration/Registration.jsx
+++ src/Pages/Authentication/Registration/Registration.jsx
@@ -15,10 +15,11 @@ const Registration = () => {
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
-        const photo = form.photo.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name, photo, password, email);
+        const birthDate = form.birthDate.value;
+        const photo = form.photo.value;
+        console.log(name, photo, password, email, birthDate);
         creatUser(email, password)
             .then(result => {
                 console.log(result);
@@ -36,9 +37,6 @@ const Registration = () => {
             .catch(error => {
                 alert(error.message)
             })
-
-
-
     }
 
     return (
@@ -49,7 +47,6 @@ const Registration = () => {
                         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 bg-opacity-90">
                             <div className="card-body">
                                 <form onSubmit={handelRegster}>
-
                                     <div className="form-control">
                                         <label className="label">
                                             <span className="label-text">Name</span>
@@ -57,13 +54,11 @@ const Registration = () => {
                                         <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered" />
 
                                     </div>
-
                                     <div className="form-control">
                                         <label className="label">
                                             <span className="label-text">Email</span>
                                         </label>
                                         <input type="email" placeholder="Enter Your Email" name="email" className="input input-bordered" />
-
                                     </div>
                                     <div className="form-control">
                                         <label className="label">
@@ -72,19 +67,19 @@ const Registration = () => {
                                         <input type="password" placeholder="Enter Your Password" name="password" className="input input-bordered" />
 
                                     </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Birth Date</span>
+                                        </label>
+                                        <input type="date" placeholder="Enter Your Birth Date" name="birthDate" className="input input-bordered" />
+                                    </div>
                                     <div className="form-control mt-5">
                                         <input type="file" name="photo" className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
-
-
                                     </div>
 
-
                                     <label className="label">
-
                                         <p> <span>if you have an account please  </span> <Link className="link-hover ml-10 text-xl text-purple-600" to='/login'>Login</Link></p>
                                     </label>
-
-
                                     <div className="form-control mt-6">
                                         <button className="btn text-white bg-purple-800 ">Regster</button><br />
                                     </div>
--- src/Pages/Provider/AuthProvider.jsx
+++ src/Pages/Provider/AuthProvider.jsx
@@ -1,64 +1,70 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile,} from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, updateProfile, } from "firebase/auth";
 import { app } from "../Firebase/firebase.config";
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
-    const [user ,setUser]= useState(null)
-    const [loading,setLoading]= useState(true)
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     //user Regster code
-    const creatUser =(email,password)=>{
+    const creatUser = (email, password) => {
         setLoading(true)
-        return createUserWithEmailAndPassword(auth,email,password);
+        return createUserWithEmailAndPassword(auth, email, password);
 
     }
 
     // Set user code
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth,user =>{
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
             setUser(user)
-           setLoading(false)
+            setLoading(false)
         })
-        return()=>{
+        return () => {
             return unsubscribe();
         }
-    },[])
+    }, [])
 
     // Login user code
-    const loaginUser = (email,password)=>{
+    const loaginUser = (email, password) => {
         setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
 
     // Updet user code
-    const updateUser = (name,photo)=>{
-        return updateProfile(auth.currentUser,{
-            displayName:name ,photoURL:photo
+    const updateUser = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
         })
     }
     // LogOut code 
 
-    const logout = ()=>{
+    const logout = () => {
         signOut(auth)
+    }
+    // reset Password 
+    const restpassword = (email) => {
+        return sendPasswordResetEmail(auth, email)
+        
     }
 
 
 
-    const authinfo ={
+    const authinfo = {
         user,
         loading,
         creatUser,
         loaginUser,
         updateUser,
-        logout
+        logout,
+        restpassword
 
     }
     return (
         <AuthContext.Provider value={authinfo}>
-          { children }
+            {children}
         </AuthContext.Provider>
     );
 };
