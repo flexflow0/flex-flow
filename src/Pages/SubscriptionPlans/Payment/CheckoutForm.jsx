626fe5e80f8fb13005e1f0bbf24957ae88bc6a3d
masudra <masudrana6278312@gmail.com>
2023-08-28 00:19:57 +0600

    link change

--- src/Pages/SubscriptionPlans/Payment/CheckoutForm.jsx
+++ src/Pages/SubscriptionPlans/Payment/CheckoutForm.jsx
@@ -46,7 +46,7 @@ const CheckForm = ({ setDisable }) => {
     const [repayment, setRepayment] = useState(false)
     useEffect(() => {
         if (totalPrice > 0) {
-            axios('http://localhost:5000create-payment-intent')
+            axios('http://localhost:5000/create-payment-intent')
                 .then(res => {
                     setClientSecret(res.data.clientSecret);
                     console.log(res.data.clientSecret);
