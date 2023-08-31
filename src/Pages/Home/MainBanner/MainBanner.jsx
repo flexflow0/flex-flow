40a33cd5e7ee9548dfa08ea1c2d8ff6695898a2c
Mahmud sajib <mahmudsajib082@gmail.com>
2023-09-01 02:38:03 +0600

    main banner

--- src/Pages/Home/MainBanner/MainBanner.jsx
+++ src/Pages/Home/MainBanner/MainBanner.jsx
@@ -1,28 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import axios from 'axios';
 import './MainBanner.css'
-import searchIcon from '../../../assets/Vector.svg'
+function MainBanner() {
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
 
-const MainBanner = () => {
-    return (
-        <div
-            className="w-full h-[60vh] text-white text-center flex flex-col justify-center space-y-8 blo rounded-sm mainBanner"
-        >
-            <h1 className='font-bold text-3xl md:text-5xl'>
-                Adventures Unveiled: <br />
-                Exploring Life&apos;s Wonders with
-            </h1>
-            <h3 className='font-semibold text-xl md:text-2xl'>
-                Watch anywhere. Cancel anytime. <br />
-                Ready to watch? Enter your email to create or restart your membership.
-            </h3>
-            <div className='relative mx-auto mb-20 w-full sm:w-96 md:w-[500px] '>
-                {/* Search Name */}
-                <input required className='pl-4 h-10 w-full sm:w-96 md:w-[500px] bg-transparent border-2 bg-[#3E3E3E]  border-[#830fea] input rounded-full text-[#A1A1A1]' type="email" placeholder='Explore Your Desire Content...' />
-                {/* Search button */}
-                <img className="absolute top-3 right-5 drop-shadow-md transition-all duration-200  disabled" height="20px" width="20px" src={searchIcon} alt="search icon" />
-            </div>
-        
-        </div>
-    );
-};
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    axios.get('http://localhost:5000/upcomingmovies')
+      .then(response => {
+        setUpcomingMovies(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="relative">
+      <Slider {...settings}>
+        {upcomingMovies.map(movie => (
+          <div key={movie._id} className="sliderImgHeight flex items-center justify-center h-300px">
+            <img
+              src={movie.movie_thum}
+              alt={movie.movie_name}
+              className="max-h-00px w-full object-cover"
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
 
 export default MainBanner;
