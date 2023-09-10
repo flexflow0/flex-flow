ca5f445da6a4c976ff6b2d407c88618f6647e2e7
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2023-09-10 22:51:07 +0600

    push the error file dont marge it

--- src/Pages/SubscriptionPlans/Payment/SSLCOMMERZ/SSLCommerzSuccess.test.js
+++ src/Pages/SubscriptionPlans/Payment/SSLCOMMERZ/SSLCommerzSuccess.test.js
@@ -1,8 +1,14 @@
-// import SSLCommerzSuccess from "./SSLCommerzSuccess";
+import { configure } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import SSLCommerzSuccess from "./SSLCommerzSuccess";
+import React from "react";
+configure({ adapter: new Adapter() });
 
-// describe('Name of the group', () => {
-//     test('should ', () => {
-//         expect().toEqual("payment Successful");
-//     });
-
-// });
+import { shallow } from 'enzyme';
+describe('SSLCommerz Testing', () => {
+    it('Payment Success ', () => {
+        const wrapper = shallow(<SSLCommerzSuccess />)
+        // console.log(wrapper.debug());
+        expect(wrapper.exists('.text-center')).toEqual(true)
+    });
+});
