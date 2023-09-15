957299b5b1a20bf1b1edd1e6895935c5a0bfe237
masudra <masudrana6278312@gmail.com>
2023-09-15 16:06:40 +0600

    uplode movies and uplode tv serise update

--- src/Pages/Dashboard/AdminDashboard/UploadMovies/UploadMovies.jsx
+++ src/Pages/Dashboard/AdminDashboard/UploadMovies/UploadMovies.jsx
@@ -16,75 +16,98 @@ const UploadMovies = () => {
             <div className="p-10">
                 <form onSubmit={handelRegister}>
                     {/* ********** */}
-                    <div className="grid grid-cols-2 gap-5">
+                    <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-5">
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Name</span>
+                                <span className="label-text">Movie Title</span>
                             </label>
-                            <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered  rounded-lg " required />
+                            <input type="text" placeholder="Enter Your Movie Name" name="name" className="input input-bordered  rounded-lg " required />
                         </div>
+
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Name</span>
+                                <span className="label-text">Release Year</span>
                             </label>
-                            <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered  rounded-lg " required />
+                            <input type="date" placeholder="Enter Your " name="name" className="input input-bordered  rounded-lg " required />
                         </div>
+
+                    </div>
+                    {/* ********** */}
+                    <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-5">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Type</span>
+                            </label>
+                            <input type="text" placeholder="Type" name="name" className="input input-bordered rounded-lg " required />
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Trailer Url</span>
+                            </label>
+                            <input type="text" placeholder="Trailer Url" name="name" className="input input-bordered rounded-lg " required />
+                        </div>
+
                     </div>
                     {/* ********* */}
                     {/* ********** */}
-                    <div className="grid grid-cols-2 gap-5">
+                    <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-5">
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Name</span>
+                                <span className="label-text">Production Company</span>
                             </label>
-                            <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered rounded-lg " required />
+                            <input type="text" placeholder="Production Company" name="name" className="input input-bordered rounded-lg " required />
                         </div>
+
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Name</span>
+                                <span className="label-text">Genres</span>
                             </label>
-                            <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered rounded-lg " required />
+                            <input type="text" placeholder="Genres" name="name" className="input input-bordered rounded-lg " required />
                         </div>
-                    </div>
-                    {/* ********* */}
-                    {/* ********** */}
-                    <div className="grid grid-cols-2 gap-5">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered rounded-lg " required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Name</span>
-                            </label>
-                            <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered rounded-lg " required />
-                        </div>
+
                     </div>
                     {/* ********* */}
 
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Name</span>
+                            <span className="label-text">Region</span>
                         </label>
-                        <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered rounded-lg " required />
+                        <input type="text" placeholder="Region" name="name" className="input input-bordered rounded-lg " required />
                     </div>
 
 
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Name</span>
+                            <span className="label-text">Description
+</span>
                         </label>
-                        <input type="text" placeholder="Enter Your Name" name="name" className="input input-bordered rounded-lg h-40 " required />
+                        <input type="text" placeholder="Description
+" name="name" className="input input-bordered rounded-lg h-40 " required />
                     </div>
 
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Name</span>
+                            <span className="label-text">Poster</span>
                         </label>
                         <input type="file" className="file-input file-input-bordered file-input bg-purple-600 w-full max-w-xs rounded-lg" />
                     </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Thumbnail
+</span>
+                        </label>
+                        <input type="file" className="file-input file-input-bordered file-input bg-purple-600 w-full max-w-xs rounded-lg" />
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Movie</span>
+                        </label>
+                        <input type="file"  className="file-input file-input-bordered file-input  bg-purple-600 w-full max-w-xs rounded-lg" />
+                    </div>
+
                     <div className="form-control mt-6">
                         <button className="btn text-white bg-purple-800 rounded-lg ">Upload Now</button><br />
                     </div>
