cb341b8038bca2f75948a0b88918e776d6efe2d2
masudra <masudrana6278312@gmail.com>
2023-09-21 15:16:41 +0600

    test

--- src/Pages/ShowAllMovies/ShowAllMovies.jsx
+++ src/Pages/ShowAllMovies/ShowAllMovies.jsx
@@ -1,52 +1,65 @@
 import { useParams } from "react-router-dom";
 import MovieCard from "../Shared/MovieCard/MovieCard";
 import useMovies from "../../Hooks/useMovies/useMovies";
-import { useContext, useState, useEffect } from "react";
-import { AuthContext } from "../Provider/AuthProvider";
-import useUser from "../../Hooks/useUser/useUser";
 import { FaSearch } from 'react-icons/fa';
 
+import { useState } from "react";
+
 const ShowAllMovies = () => {
-    const { user, loading } = useContext(AuthContext);
-    const { keyword } = useParams();
-    const [search, setSearch] = useState("");
 
-    if(loading){
-        return;
+    let queries = {
+        genre: undefined,
+        region: undefined,
     }
-    const [userData] = useUser(user.email)
 
-    const queries = {
-        genre: keyword.startsWith('genres') ? keyword.split('+')[1] : undefined,
-        region: keyword.startsWith('regions') ? keyword.split('+')[1] : undefined,
-    };
+    const { keyword } = useParams();
+    const keyValues = keyword.split('+');
+    const [search, setSearch] = useState("");
+    console.log(search)
+    // console.log(keyword, keyValues);
 
-    const [movies] = useMovies(queries, userData?.birthDate);
-    const sortedByRating = [...movies].sort((a, b) => b.IMDb_rating - a.IMDb_rating);
-    const newMovies = [...movies].reverse();
+    if (keyValues[0] === 'regions') {
+        queries.region = keyValues[1];
+    }
+    else if (keyValues[0] === 'genres') {
+        queries.genre = keyValues[1];
+    }
+    // console.log(queries);
+
+    const [movies] = useMovies(queries);
+    const sortedByRating = movies.slice(0);
+    sortedByRating.sort(function (a, b) {
+        return b.IMDb_rating - a.IMDb_rating;
+    });
+    const newMovies = movies.slice(0);
+    newMovies.reverse();
+
     const allMovies = (keyword === 'top_rated') ? sortedByRating : newMovies;
 
+
+
+
+    console.log(movies)
     return (
         <div>
             <div className="flex justify-center items-center mt-16 gap-4">
 
                 <div className='relative mx-auto px-5 mb-20 w-full sm:w-96 md:w-[500px] '>
-                    {/* Subscribe Email */}
-<input required className='pl-6  py-5 h-10 w-full sm:w-96 md:w-[500px] bg-transparent border-2 bg-[#3E3E3E] border-[#830fea] input rounded-full text-white' type="search" placeholder="Search Movies" onChange={(e) => setSearch(e.target.value)} />
                     <input required className='pl-6  py-5 h-10 w-full sm:w-96 md:w-[500px] bg-transparent border-2 bg-[#3E3E3E] border-[#830fea] input rounded-full text-white' type="search" placeholder="Search Movies" onChange={(e) => setSearch(e.target.value)} />
-                    {/* Subscribe button */}
                         <button className='absolute top-1/2 -right-3 -translate-y-1/2  px-4 text-white'><FaSearch></FaSearch></button>
                 
                 </div>
+             
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 p-5 my-20">
-                {allMovies
-                    .filter((movie) =>
-                        search.toLowerCase() === "" ? true : movie.title.toLowerCase().includes(search)
-                    )
-                    .map((movie) => (
-                        <MovieCard key={movie._id} movie={movie}></MovieCard>
-                    ))}
+                {
+                    allMovies.filter((movie) => {
+                        return search.toLowerCase() === "" ? movie : movie.title.toLowerCase().includes(search);
+                    }).map(movie => <MovieCard
+                        key={movie._id}
+                        movie={movie}
+                    ></MovieCard>)
+                }
             </div>
         </div>
     );
